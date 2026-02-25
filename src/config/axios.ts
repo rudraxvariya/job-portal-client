b524b3c76139f96d3f25711389fc2bd9470420b7
variyarudrax <rudrax.variya@bacancy.com>
2026-02-25 15:50:55 +0530

    Update Axios configuration to use environment variable for base URL

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL;
+
 export const axiosInstance = axios.create({
-  baseURL: "/api/v1",
+  baseURL,
   withCredentials: true, // send cookies with requests (e.g. auth token)
 });
